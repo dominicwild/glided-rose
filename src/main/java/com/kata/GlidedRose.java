f9025681f76c5e5f99751b23679c5ba8824a3985
dwild8 <dwild8@dxc.com>
2021-12-22 01:54:38 +0000

    Method extraction

--- src/main/java/com/kata/GlidedRose.java
+++ src/main/java/com/kata/GlidedRose.java
@@ -5,7 +5,7 @@ class GildedRose {
 	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
 	private static final String AGED_BRIE = "Aged Brie";
-	
+
 	Item[] items;
 
 	public GildedRose(Item... items) {
@@ -15,52 +15,63 @@ class GildedRose {
 	public void updateQuality() {
 		for (int i = 0; i < items.length; i++) {
 			Item item = items[i];
-			if (!item.name.equals(AGED_BRIE)
-					&& !item.name.equals(BACKSTAGE_PASSES)) {
-				if (item.quality > 0) {
-					if (!item.name.equals(SULFURAS)) {
-						item.quality = item.quality - 1;
+
+			updateQualityBeforeSellByDate(item);
+			updateQualityAfterSellByDate(item);
+			updateSellIn(item);
+		}
+	}
+
+	private void updateQualityBeforeSellByDate(Item item) {
+		if (!item.name.equals(AGED_BRIE)
+				&& !item.name.equals(BACKSTAGE_PASSES)) {
+			if (item.quality > 0) {
+				if (!item.name.equals(SULFURAS)) {
+					item.quality = item.quality - 1;
+				}
+			}
+		} else {
+			if (item.quality < 50) {
+				item.quality = item.quality + 1;
+
+				if (item.name.equals(BACKSTAGE_PASSES)) {
+					if (item.sellIn < 11) {
+						if (item.quality < 50) {
+							item.quality = item.quality + 1;
+						}
 					}
+
+					if (item.sellIn < 6) {
+						if (item.quality < 50) {
+							item.quality = item.quality + 1;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private void updateSellIn(Item item) {
+		if (!item.name.equals(SULFURAS)) {
+			item.sellIn = item.sellIn - 1;
+		}
+	}
+
+	private void updateQualityAfterSellByDate(Item item) {
+		if (item.sellIn < 0) {
+			if (!item.name.equals(AGED_BRIE)) {
+				if (!item.name.equals(BACKSTAGE_PASSES)) {
+					if (item.quality > 0) {
+						if (!item.name.equals(SULFURAS)) {
+							item.quality = item.quality - 1;
+						}
+					}
+				} else {
+					item.quality = item.quality - item.quality;
 				}
 			} else {
 				if (item.quality < 50) {
 					item.quality = item.quality + 1;
-
-					if (item.name.equals(BACKSTAGE_PASSES)) {
-						if (item.sellIn < 11) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
-
-						if (item.sellIn < 6) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
-					}
-				}
-			}
-
-			if (!item.name.equals(SULFURAS)) {
-				item.sellIn = item.sellIn - 1;
-			}
-
-			if (item.sellIn < 0) {
-				if (!item.name.equals(AGED_BRIE)) {
-					if (!item.name.equals(BACKSTAGE_PASSES)) {
-						if (item.quality > 0) {
-							if (!item.name.equals(SULFURAS)) {
-								item.quality = item.quality - 1;
-							}
-						}
-					} else {
-						item.quality = item.quality - item.quality;
-					}
-				} else {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
 				}
 			}
 		}
