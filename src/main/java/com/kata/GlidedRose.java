52285efb9d7038c3a0a53df89bac78f8c527350d
dwild8 <dwild8@dxc.com>
2021-12-22 01:57:33 +0000

    Break up else to begin reducing indentation

--- src/main/java/com/kata/GlidedRose.java
+++ src/main/java/com/kata/GlidedRose.java
@@ -30,7 +30,9 @@ class GildedRose {
 					item.quality = item.quality - 1;
 				}
 			}
-		} else {
+		}
+		if (item.name.equals(AGED_BRIE)
+				|| item.name.equals(BACKSTAGE_PASSES)) {
 			if (item.quality < 50) {
 				item.quality = item.quality + 1;
 
